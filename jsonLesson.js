4428925b8a0966b188700335a456f4dea63f178e
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-07-23 17:14:22 -0400

    changed everything

--- jsonLesson.js
+++ jsonLesson.js
@@ -1,9 +1,47 @@
+function knicksStarters(){
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.knicks.startingLineUp + " " + myObj.knicks.frank.born;
+
+        var a = document.getElementById("starters");
+        
+        a.innerHTML = myObj.knicks.startingLineUp;
+
     }
 };
 xmlhttp.open("GET", "https://raw.githubusercontent.com/awalia1/JSON/master/knicks.json", true);
 xmlhttp.send();
+}
+
+function frankBirth(){
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+
+        var a = document.getElementById("frankBirthday");
+        
+        a.innerHTML = myObj.knicks.frank.born;
+
+    }
+};
+xmlhttp.open("GET", "https://raw.githubusercontent.com/awalia1/JSON/master/knicks.json", true);
+xmlhttp.send();
+}
+
+function knoxBirth(){
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+
+        var a = document.getElementById("knoxBirthday");
+        
+        a.innerHTML = myObj.knicks.knox.born;
+
+    }
+};
+xmlhttp.open("GET", "https://raw.githubusercontent.com/awalia1/JSON/master/knicks.json", true);
+xmlhttp.send();
+}
